5d29d0acb63e8dfb03ccd2f8354dfa9e28dcf874
Iván Luna <ivanflowtramites@gmail.com>
2024-12-25 17:59:36 -0300

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import astroExpressiveCode from 'astro-expressive-code'
+import react from '@astrojs/react' 
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  integrations: [
+    tailwind(),
+    astroExpressiveCode({
+      themes: ['poimandres', 'light-plus'],
+      styleOverrides: {
+        borderRadius: '0.5rem',
+        borderWidth: '0px',
+      },
+    }),
+    react(), 
+  ],
+})
